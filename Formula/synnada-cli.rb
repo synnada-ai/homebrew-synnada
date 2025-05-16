3b87bc66c8d88c0391cdf1d805b25fb83b1a793f
ertunc-synnada <ertunc.efe@synnada.ai>
2025-05-16 14:54:49 +0300

    formula updated

--- Formula/synnada-cli.rb
+++ Formula/synnada-cli.rb
@@ -2,8 +2,8 @@
 class SynnadaCli < Formula
   desc "Synnada CLI tool"
   homepage "https://github.com/synnada-ai/cli"
-  url "https://synnada-ai.github.io/homebrew-synnada/releases/cli/cli-v0.0.28.tar.gz"
-  sha256 "b9a6091096c49a78aca55b02fae376130383685ea3faf4856bb7cd67851f4b21"
+  url "https://synnada-ai.github.io/homebrew-synnada/releases/cli/cli-v#{version}.zip"
+  sha256 "<AUTO-BUMPED_SHA256>"
   license "MIT"
 
   def install
