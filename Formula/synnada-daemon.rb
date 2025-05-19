9095e51a97a9498f38bcbf9f3bf1b81685d6b1a1
ertunc-synnada <ertunc.efe@synnada.ai>
2025-05-19 23:42:33 +0300

    home brew python dependecy added to daemon formula

--- Formula/synnada-daemon.rb
+++ Formula/synnada-daemon.rb
@@ -1,16 +1,28 @@
 class SynnadaDaemon < Formula
     desc "Synnada daemon"
     homepage "https://github.com/synnada-ai/daemon"
-    url "https://synnada-ai.github.io/homebrew-synnada/releases/daemon/synnadad-v0.0.1.zip"
+    url "https://synnada-ai.github.io/homebrew-synnada/releases/daemon/synnadad-v#{version}.zip"
     sha256 "<AUTO-BUMPED_SHA256>"
     license "MIT"
   
+    # Runtime dependencies:
+    depends_on "python@3.11"
+    depends_on "librdkafka"
+  
     def install
       bin.install "synnadad"
+  
+      # Rewrite the embedded Python.framework path to point at Homebrew's version
+      python_framework = "#{Formula["python@3.11"].opt_prefix}/Frameworks/Python.framework/Versions/3.11/Python"
+      odie "Missing Homebrew Python framework!" unless File.exist?(python_framework)
+  
+      system "install_name_tool", "-change",
+             "/Library/Frameworks/Python.framework/Versions/3.13/Python",
+             python_framework,
+             bin/"synnadad"
     end
   
     test do
-      # e.g. check exit code or version
       assert_match version.to_s, shell_output("#{bin}/synnadad --version")
     end
   end
