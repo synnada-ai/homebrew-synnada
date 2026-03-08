a018f727757dcc55b2ce9ebbec3e8d55e28239c1
SamSynnada <samican@synnada.ai>
2026-03-08 17:51:49 +0300

    Add agency formula (v0.1.0-rc2)

---
+++ Formula/agency.rb
@@ -0,0 +1,20 @@
+class Agency < Formula
+  include Language::Python::Virtualenv
+
+  desc "Agency CLI runtime host"
+  homepage "https://github.com/synnada-ai/agency-cli-dist"
+  url "https://github.com/synnada-ai/agency-cli-dist/releases/download/v0.1.0-rc2/agency_cli-0.1.0-py3-none-any.whl"
+  sha256 "5a01ba2060d685ea12fca3092a20305203545ac244555da00e85275951ee5ca0"
+  license "Proprietary"
+
+  depends_on "python@3.14"
+
+  def install
+    venv = virtualenv_create(libexec, Formula["python@3.14"].opt_bin/"python3.14")
+    venv.pip_install_and_link cached_download
+  end
+
+  test do
+    assert_match "Usage: agency", shell_output("#{bin}/agency --help")
+  end
+end
